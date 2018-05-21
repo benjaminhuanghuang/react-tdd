d3eaa4333722fb71a9aaeb2c524387116fb6db30
Benjamin Huang <bhuang@rms.com>
2018-05-21 15:31:09 -0700

    updaste

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,11 @@ import './App.css';
 import Calculator from '../Calculator/Calculator';
 
 const App = () => {
-    return (
-      <div className="app-container">
-        <Calculator />
-      </div>
-    );
-  }
+  return (
+    <div className="app-container">
+      <Calculator />
+    </div>
+  );
+}
 
 export default App;
-
-App
-Warning: React.createElement: type is invalid -- expected a string (for built-in components) or a class/function (for composite components) but got: object. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.
